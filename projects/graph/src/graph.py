6f452362986dca9a9cb6e91b72b1ce2434f3e210
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-07-24 14:30:44 -0400

    Minimal working graph solution
    - Add Graph class in graph.py and stub for Vertex class
      - Add add_vertex method with error handling
      - Add add_edge method with error handling
    - Add minimal working code for BokehGraph method
      - Randomize color and position of vertexes
      - Add some test code that will generate a graph when draw.py is run

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,7 +5,33 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
+class Vertex:
+    """Individual Vertex class"""
+    def __init__(self, label):
+        self.label = label
+
+
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """Represent a graph as a dict of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex, edges=()):
+        """ Add a new vertex, optionally with edges to other vertices"""
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
+
+    def add_edge(self, start, end, bidirectional=True):
+        """Add a edge (default bidirectional) between to vertices"""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Vertices to connect not in graph!')
+
+        self.vertices[start].add(end)
+
+        if bidirectional:
+            self.vertices[end].add(start)
+
+
